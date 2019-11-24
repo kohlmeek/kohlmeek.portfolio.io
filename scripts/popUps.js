e53a65dda2b761a05cfce445b45d27787ac1cfe3
kohlmeek <33040204+kohlmeek@users.noreply.github.com>
2019-11-24 12:52:51 -0800

    Add files via upload

---
+++ scripts/popUps.js
@@ -0,0 +1,26 @@
+function popUps() {
+    let counter=1;
+    if (confirm("You seem to be trying to leave this totally kickass page. push ok to confirm") && (counter=1))
+    {
+    if (confirm("It seems you clicked ok when you meant to click cancel. Let's try that again"))
+    {
+      if (confirm("No cancel is the one that let's you stay on the page"))
+    {
+      if (confirm("Why do you want so badly to leave"))
+                {
+                if (confirm("Am I really that terrible?"))
+                    {
+                    if (confirm("Oh... I see"))
+                        {
+                        if (confirm("Fine Well fuck you. who needs you anyway!"))
+                        {
+                                counter=couner-1;
+                           
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+}
